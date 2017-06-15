43b2efda099364c7bf408a8979920b4bf2918547
Klemen Kosir <klemen.kosir@icloud.com>
2017-06-15 09:52:25 +0200

    + added URLSessionExtension

---
+++ SwiftExtensions/SwiftExtensions/URLSessionExtension.swift
@@ -0,0 +1,55 @@
+//
+//  URLSessionExtension.swift
+//  SwiftExtensions
+//
+//  Created by Klemen Kosir on 15. 06. 17.
+//  Copyright © 2017 Klemen Kosir. All rights reserved.
+//
+
+import Foundation
+
+extension URLSession {
+	
+	func synchronousDataTask(with url: URL) -> (Data?, URLResponse?, Error?) {
+		var data: Data?
+		var response: URLResponse?
+		var error: Error?
+		
+		let semaphore = DispatchSemaphore(value: 0)
+		
+		let dataTask = self.dataTask(with: url) {
+			data = $0
+			response = $1
+			error = $2
+			
+			semaphore.signal()
+		}
+		dataTask.resume()
+		
+		_ = semaphore.wait(timeout: .distantFuture)
+		
+		return (data, response, error)
+	}
+	
+	func synchronousDataTask(with request: URLRequest) -> (Data?, URLResponse?, Error?) {
+		var data: Data?
+		var response: URLResponse?
+		var error: Error?
+		
+		let semaphore = DispatchSemaphore(value: 0)
+		
+		let dataTask = self.dataTask(with: request) { (d, r, e) in
+			data = d
+			response = r
+			error = e
+			
+			semaphore.signal()
+		}
+		dataTask.resume()
+		
+		_ = semaphore.wait(timeout: .distantFuture)
+		
+		return (data, response, error)
+	}
+	
+}
